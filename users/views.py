1678798811fdc9668a6b6a8891f4d8bf7f53d505
Uliana Semyonova <i@ulianasemyonova.ru>
2024-02-07 16:02:36 +0500

    Create register, login and logout for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class RegisterCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+
+    def get_success_url(self):
+        return reverse('main:survey_list')
