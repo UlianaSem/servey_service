6528321a314b2b1fa8cde1e4fb26f3c413958d61
Uliana Semyonova <i@ulianasemyonova.ru>
2024-02-07 13:24:06 +0500

    Create admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Question, Survey, Answer
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    inlines = [AnswerInline]
+
+
+@admin.register(Question)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'survey', 'text', 'question', 'answer']
+    search_fields = ['id', 'name', 'survey', 'text']
+    list_filter = ['survey']
+    inlines = [AnswerInline]
+
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['id', 'name']
+    inlines = [QuestionInline]
